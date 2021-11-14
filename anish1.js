cd9b81fee1fca6080c0d1a4b0d63a33216e0b0d7
Anish Kumar Sharma <80304547+Anish1430@users.noreply.github.com>
2021-11-14 20:58:13 +0530

    Add files via upload

---
+++ anish1.js
@@ -0,0 +1,21 @@
+//   array index 
+// let array=[1,2,3,4,5,6];
+// console.log(array) 
+
+
+// using parameter
+// function array1(paraa, parab) {
+//     let arr = new Array();
+//     for (let i= paraa; i<=parab; i++){
+//         arr.push(i);
+//     }
+//     return arr;
+// }
+
+// let arr = array1(4, 10);
+// console.log(arr)
+
+
+//How to find length of the array//
+// let array=[1,2,3,4,5,6,7,8];
+// console.log(array.length);
